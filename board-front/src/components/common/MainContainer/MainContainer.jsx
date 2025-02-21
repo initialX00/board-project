2ebd9c394495788da9c4a77856c1cd9970b35c8d
initialX00 <rkdlswlsdn@naver.com>
2025-02-21 18:03:23 +0900

    2월21일

---
+++ board-front/src/components/common/MainContainer/MainContainer.jsx
@@ -0,0 +1,33 @@
+/**@jsxImportSource @emotion/react */
+import { useRecoilState } from 'recoil';
+import * as s from './style';
+import React from 'react';
+import { mainSidebarIsOpenState } from '../../../atoms/mainSidebar/mainSidebarAtom';
+import { FiChevronsRight } from 'react-icons/fi';
+import { basicButton } from '../../../styles/buttons';
+
+function MainContainer({ children }) {
+    const [ isOpen, setOpen ] = useRecoilState(mainSidebarIsOpenState);
+
+    const handleSidebarOpen = () => {
+        setOpen(true)
+    }
+
+    return (
+        <div css={s.container}>
+            <header css={s.header}>
+                {
+                    !isOpen &&
+                    <span css={s.sidebarOpenButton}>
+                        <button css={basicButton} onClick={handleSidebarOpen}><FiChevronsRight /></button>
+                    </span>
+                }
+            </header>
+            <main css={s.main}>
+                {children}
+            </main>
+        </div>
+    );
+}
+
+export default MainContainer;
